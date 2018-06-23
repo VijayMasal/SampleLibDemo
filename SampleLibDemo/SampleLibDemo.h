ee39794eda69cd383c73dc8a1645bbcf4b3a5c7a
vijay <vdmasal@gmail.com>
2018-06-23 16:55:26 +0530

    Initial Commit

---
+++ SampleLibDemo/SampleLibDemo.h
@@ -0,0 +1,19 @@
+//
+//  SampleLibDemo.h
+//  SampleLibDemo
+//
+//  Created by Emberify_Vijay on 23/06/18.
+//  Copyright © 2018 Vijay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleLibDemo.
+FOUNDATION_EXPORT double SampleLibDemoVersionNumber;
+
+//! Project version string for SampleLibDemo.
+FOUNDATION_EXPORT const unsigned char SampleLibDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleLibDemo/PublicHeader.h>
+
+
